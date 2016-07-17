d7bd0077930f61bb3b372d85e8d60d8cdb06172a
probablyirina <irina.branovic@gmail.com>
2016-07-17 20:46:33 +0200

    Trying with REST

--- src/main/java/rs/personaltrainer/RecordController.java
+++ src/main/java/rs/personaltrainer/RecordController.java
@@ -3,11 +3,8 @@ package rs.personaltrainer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class RecordController {
