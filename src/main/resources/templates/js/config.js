9e601d96ad1054a979e7dcfc818f2015d99a9425
probablyirina <irina.branovic@gmail.com>
2016-07-17 20:21:21 +0200

    Trying with Thymeleaf

---
+++ src/main/resources/templates/js/config.js
@@ -0,0 +1,34 @@
+require.config({
+  shim: {
+
+  },
+  paths: {
+    bootstrap: "lib/bootstrap/dist/js/bootstrap",
+    "bootstrap-switch": "lib/bootstrap-switch/dist/js/bootstrap-switch",
+    "bootstrap-toggle": "lib/bootstrap-toggle/js/bootstrap-toggle.min",
+    colors: "lib/colors/gulpfile",
+    grunt: "lib/grunt/lib/grunt",
+    jqBootstrapValidation: "lib/jqBootstrapValidation/dist/jqBootstrapValidation-1.3.7.min",
+    "jquery-birthday-picker": "lib/jquery-birthday-picker/jquery.birthday.picker",
+    "jquery-migrate": "lib/jquery-migrate/jquery-migrate",
+    "jquery-ui": "lib/jquery-ui/jquery-ui",
+    "jquery.cookie": "lib/jquery.cookie/jquery.cookie",
+    "jquery.easing": "lib/jquery.easing/js/jquery.easing",
+    "jquery.fitvids": "lib/jquery.fitvids/jquery.fitvids",
+    "jquery.nicescroll": "lib/jquery.nicescroll/jquery.nicescroll",
+    knockout: "lib/knockout/dist/knockout",
+    "nivo-lightbox": "lib/nivo-lightbox/nivo-lightbox",
+    "owl.carousel": "lib/owl.carousel/dist/owl.carousel",
+    parallax: "lib/parallax/deploy/parallax.min",
+    "jquery.parallax": "lib/parallax/deploy/jquery.parallax.min",
+    requirejs: "lib/requirejs/require",
+    respond: "lib/respond/dest/respond.src",
+    "responsive.ie10mobilefix": "lib/responsive/build/responsive.ie10mobilefix.min",
+    responsive: "lib/responsive/build/responsive.min",
+    slicknav: "lib/slicknav/dist/jquery.slicknav",
+    underscore: "lib/underscore/underscore"
+  },
+  packages: [
+
+  ]
+});
