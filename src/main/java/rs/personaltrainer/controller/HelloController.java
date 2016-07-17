0035d2ffd08f3f88e22c347dc16ed57bb43a84f6
probablyirina <irina.branovic@gmail.com>
2016-07-17 12:33:44 +0200

    Spring boot conf

--- src/main/java/rs/personaltrainer/controller/HelloController.java
+++ src/main/java/rs/personaltrainer/controller/HelloController.java
@@ -1,14 +1,15 @@
 package rs.personaltrainer.controller;
 
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping(value="html", method = RequestMethod.GET)
     public String index() {
-        return "/static/index.html";
+        return "index.html";
     }
 
 }
